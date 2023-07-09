a845522be36cb363671c600fa1558dcc77da5856
Akın Cem <79974860+akincemtutal9@users.noreply.github.com>
2023-07-09 20:35:42 +0300

    feat: player attributes

---
+++ Kavakli-Bird/Assets/Project/Scripts/Player.cs
@@ -0,0 +1,23 @@
+
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private string name { get; set; }
+
+    [SerializeField] private AudioClip dieClip {get; set;}
+
+    [SerializeField] private AudioClip jumpClip { get; set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Kavakli-Bird/Assets/Project/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Lean.Touch;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float jumpHeight;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
