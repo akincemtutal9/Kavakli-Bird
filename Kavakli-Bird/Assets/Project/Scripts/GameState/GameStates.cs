d0fa44c21c45185f5041dd4bbac585a1a69229eb
Akın Cem <79974860+akincemtutal9@users.noreply.github.com>
2023-07-12 05:19:14 +0300

    fix: game state code refactor

--- Kavakli-Bird/Assets/Project/Scripts/GameState/GameStates.cs
+++ Kavakli-Bird/Assets/Project/Scripts/GameState/GameStates.cs
@@ -4,14 +4,12 @@ public class GameStates : MonoBehaviour
 {
     public static GameStates Instance;
 
-    private GameState gameState1;
+    private GameState gameState;
 
-    [SerializeField] private GameObject Game;
-    [SerializeField] private GameObject MainMenu;
-    [SerializeField] private GameObject DieMenu;
-    [SerializeField] private GameObject CharacterSelectMenu;
-
-    public GameState GameState1 { get => gameState1; set => gameState1 = value; }
+    [SerializeField] private GameObject game;
+    [SerializeField] private GameObject mainMenu;
+    [SerializeField] private GameObject dieMenu;
+    [SerializeField] private GameObject characterSelectMenu;
 
     public enum GameState
     {
@@ -20,75 +18,36 @@ public class GameStates : MonoBehaviour
         Die,
         CharacterSelect
     }
-    void Awake()
+
+    private void Awake()
     {
-        if(Instance == null)
+        if (Instance == null)
         {
             Instance = this;
         }
         else
         {
-            Destroy(Instance);
+            Destroy(gameObject);
         }
     }
+
     private void Start()
     {
         ChangeGameState(GameState.MainMenu);
-        
     }
-   
+
     public void ChangeGameState(GameState state)
     {
-        GameState1 = state;
-    
-        if(state == GameState.MainMenu)
-        {
-            OnMainMenu();
-        }
-        else if(state == GameState.Die)
-        {
-            OnDieMenu();
-        }
-        else if(state == GameState.CharacterSelect)
-        {
-            OnCharacterSelect();
-        }
-        else if(state == GameState.InGame)
-        {
-            OnInGame();
-        }
-        else
-        {
-            state = GameState.MainMenu;
-        }
-    }
-    public void OnInGame()
-    {
-        Game.SetActive(true);
-        DieMenu.SetActive(false);
-        MainMenu.SetActive(false);
-        CharacterSelectMenu.SetActive(false);
-    }
-    public void OnDieMenu() { 
-        DieMenu.SetActive(true);
-        MainMenu.SetActive(false);
-        CharacterSelectMenu.SetActive(false);
-        Game.SetActive(false);
+        gameState = state;
 
+        OnGameStateChange();
     }
-    public void OnMainMenu()
-    {
-        DieMenu.SetActive(false);
-        MainMenu.SetActive(true);
-        CharacterSelectMenu.SetActive(false);
-        Game.SetActive(false);
 
-    }
-    public void OnCharacterSelect()
+    private void OnGameStateChange()
     {
-        CharacterSelectMenu.SetActive(true);
-        MainMenu.SetActive(true);
-        DieMenu.SetActive(false);
-        Game.SetActive(false);
+        game.SetActive(gameState == GameState.InGame);
+        mainMenu.SetActive(gameState == GameState.MainMenu);
+        dieMenu.SetActive(gameState == GameState.Die);
+        characterSelectMenu.SetActive(gameState == GameState.CharacterSelect);
     }
 }
